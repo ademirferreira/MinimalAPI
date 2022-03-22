63005a7975be99f2f51385cf1cf711f93b46db78
Ademir Ferreira <whoisademir@gmail.com>
2022-03-21 21:32:59 -0300

    Adicionado Endpoint para retornar fornecedor por ID

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Data;
+using MinimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,18 @@ app.MapGet("/fornecedor", async (
     MinimalContextDb context) =>
     await context.Fornecedores.ToListAsync())
     .WithName("GetFornecedor")
+    .WithTags("Fornecedor");
+
+app.MapGet("/fornecedor/{id}", async (
+        Guid id,
+        MinimalContextDb context) =>
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor
+            ? Results.Ok(fornecedor)
+            : Results.NotFound())
+    .Produces<Fornecedor>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetFornecedorPorId")
     .WithTags("Fornecedor");
 
 app.Run();
