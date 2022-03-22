535518ecd44ab933f76d0fbeced4f28864ced42d
Ademir Ferreira <whoisademir@gmail.com>
2022-03-21 21:12:51 -0300

    Configuração do contexto de acesso a dados

---
+++ Models/Fornecedor.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalAPI.Models;
+
+public class Fornecedor
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Documento { get; set; }
+    public bool Ativo { get; set; }
+}
