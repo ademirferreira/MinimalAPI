e3d607f80bfa2a1409dd48ca335107125b5ba4ed
Ademir Ferreira <whoisademir@gmail.com>
2022-03-21 21:51:49 -0300

    Adicionado Endpoint Post fornecedor

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Data;
 using MinimalAPI.Models;
+using MiniValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,5 +39,25 @@ app.MapGet("/fornecedor/{id}", async (
     .Produces(StatusCodes.Status404NotFound)
     .WithName("GetFornecedorPorId")
     .WithTags("Fornecedor");
+
+app.MapPost("/fornecedor", async (
+    MinimalContextDb context,
+    Fornecedor fornecedor) =>
+    {
+        if (!MiniValidator.TryValidate(fornecedor, out var errors))
+            return Results.ValidationProblem(errors);
+
+        context.Fornecedores.Add(fornecedor);
+        var result = await context.SaveChangesAsync();
+
+        return result > 0
+            ? Results.CreatedAtRoute("GetFornecedorPorId", new { id = fornecedor.Id }, fornecedor)
+            : Results.BadRequest("Houve um problema ao salvar o registro");
+    })
+  .ProducesValidationProblem()
+  .Produces<Fornecedor>(StatusCodes.Status201Created)
+  .Produces(StatusCodes.Status400BadRequest)
+  .WithName("PostFornecedor")
+  .WithTags("Fornecedor");
 
 app.Run();
